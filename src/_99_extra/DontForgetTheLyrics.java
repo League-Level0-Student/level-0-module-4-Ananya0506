e665c27b701680b3c89891f00e840a2b0bb61551
league <league@imac-14.attlocal.net>
2023-03-04 11:27:35 -0800

    am on fruit quiz-- technically finished song lyrics thing but want to add to it at home

--- src/_99_extra/DontForgetTheLyrics.java
+++ src/_99_extra/DontForgetTheLyrics.java
@@ -6,7 +6,7 @@ import javax.swing.JApplet;
 import javax.swing.JOptionPane;
 
 public class DontForgetTheLyrics {
-	/*
+	 /*
 	 * For this game, we'll play the start of a song, and the player has to guess the rest of the lyrics.
 	 * 
 	 * 1. To record a sound clip, open Audacity and record your song.
@@ -15,25 +15,45 @@ public class DontForgetTheLyrics {
 	 * 
 	 * 3. Drag your file from the Desktop into the "default package" under "src".
 	 */
-	
+	static AudioClip audioClip;
 	public static void main(String[] args) {
 		// 4. Make a pop-up to explain the game.
-JOptionPane.showMessageDialog(null,"welcome to Guess The Song! you will hear the begginning of a song, and then you will try to guess the song.");
+int points = 0;
+		JOptionPane.showMessageDialog(null,"welcome to Guess The Song! you will hear a song, and then you will try to guess the song.if you get it right, you get a point. get as many points as you can.");
 		// 5. Use the playSound method to play your song.
-playSound();
+playSound("whatever it takes.wav");
 		// 6. Make a pop-up for the player to type their answer.
-
+String answer = JOptionPane.showInputDialog("type your guess in the space below. format: *song* by *band or singer*."); 
 		// 7. If they answered correctly, tell them that they were right.
-
+if (answer.equalsIgnoreCase("whatever it takes by imagine dragons")) {
+	JOptionPane.showMessageDialog(null, "great! you guessed it right! time for the next song guess!");
+	points ++;
+	audioClip.stop();
+} else { 
+	JOptionPane.showMessageDialog(null, "aww man, you got the answer wrong! the correct answer was 'whatever it takes by Imagine Dragons.' but it's ok, you will get another chance in the second song guess!");
+audioClip.stop();
+}
 		// 8. Otherwise, tell them they are wrong, and give them the answer.
 
 		// 9. Record another sound and repeat steps 5-8.
-
+JOptionPane.showMessageDialog(null, "alright, time for another one! are you ready???");
+playSound("titanium.wav");
+String answer2 = JOptionPane.showInputDialog("type your guess in the space below. ");
+if (answer2.equals("titanium by Sia")) {
+	JOptionPane.showMessageDialog(null, "woohoo! you got it right! time for the next one!");
+points ++;
+audioClip.stop();
+} else {
+	JOptionPane.showMessageDialog(null, "off..., you got it wrong! better luck next time!");
+audioClip.stop();
+}
 		// 10. [optional] Add a points variable that will calculate their final score.
+	JOptionPane.showMessageDialog(null, "you finished this game! your score is: " + points +". nice job, and goodbye! hope you enjoyed the game!");
+	System.exit(0);
 	}
 
 	public static void playSound(String fileName) {
-		AudioClip audioClip = JApplet.newAudioClip(new DontForgetTheLyrics().getClass().getResource(fileName));
+		audioClip = JApplet.newAudioClip(new DontForgetTheLyrics().getClass().getResource(fileName));
 		audioClip.play();
 	}
 }
--- src/_99_extra/FruitQuiz.java
+++ src/_99_extra/FruitQuiz.java
@@ -13,6 +13,7 @@ public class FruitQuiz extends KeyAdapter {
 	void makeQuestions() {
 		question1 = new JLabel("<html>Which is not a real fruit? <br> A: Dragon Fruit <br> B: Durian <br> C: Crazyberry</html>");
 		// 11. Make another question called "question2".  Use question1 above as a guide.
+		question2 = new JLabel("<html>Which fruit is acidic? <br> A: Banana <br> B: Orange <br> C: Mango</html>");
 	}
 
 	@Override
@@ -21,7 +22,9 @@ public class FruitQuiz extends KeyAdapter {
 		// 1. Print out the key code variable
 System.out.println(keyCode);
 		// 2. Make 3 int variables that hold the key codes for A, b, and C
-		
+		int a = 65;
+		int b = 66;
+		int c = 67;
 		// 14. Repeat steps 11, 12, and 13 for question3 and question4 - IMPORTANT: The questions must be in reverse order from top to bottom to work properly
 		
 		// 12. If question2 is showing,
@@ -31,16 +34,20 @@ System.out.println(keyCode);
 			
 		if (question1.isShowing()) {
 			// 3. If they selected the right fruit, do steps 4 and 7
+			if (keyCode == a) {
+				
 			
 				// 4. Call the correct() method
-				
+				correct();
 				// 7. Use the nextQuestion() method to go to question2
+			nextQuestion(question2);
 			
-			
+			}else {
 			// 8. else (if they touched something else)
 				
 				// 9. Call the incorrect() method
-		
+		incorrect();
+			}
 		}
 
 	}
